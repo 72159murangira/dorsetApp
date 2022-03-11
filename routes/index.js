a07bc5c9e7c45105aec0bf97d8b23b5d15e0caca
Wenhao Fu <wenhao.fu@gmail.com>
2022-03-11 14:46:02 +0000

    change index.js

--- routes/index.js
+++ routes/index.js
@@ -4,7 +4,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', {page:'Home', menuId:'home'});
+  res.render('index', { title: "Simple Application" });
 });
 
 
