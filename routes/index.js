6d40937d2be3b758f87c271b5e3231eacecb9bb7
Wenhao Fu <wenhao.fu@gmail.com>
2022-03-11 14:21:22 +0000

    change index.js

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,18 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.redirect("/index")
+  res.redirect("/index/add")
+});
+
+/* GET home page. */
+router.get('/add', function(req, res, next) {
+  var result = 0
+  var {num1, num2} = req.query
+  if(num1 !=='' && num2 !=='') {
+      result = utils.add(parseFloat(num1), parseFloat(num2))
+  }
+  res.render('compute', { title: 'add' ,result:result, num1, num2, symbol:"+" });
+
 });
 
 
